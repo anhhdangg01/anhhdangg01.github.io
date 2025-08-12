3716e22f0d57a7e0273b056429eccedbce2fa112
anhhdangg01 <dangaq1@uci.edu>
2025-08-12 00:45:37 -0700

    Revamped styling to home page, fixed syntax, cleared unused code/files, home accessibility on mobile WIP

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,21 +4,16 @@ import '../styles/home.css';
 function Home() {
   return (
     <div className="home">
-      <div className="info">
-        <div className="text-section">
-          <h1>Hello there!</h1>
-          <h2>My name is Anh Dang.</h2>
-        </div>
-        <div className="me">
-          <div className="about-me">
-            I am passionate about UI/UX design and art! I believe expressing oneself by creating interactable 
-            yet stunning interfaces for all kinds of people to enjoy is a wonderful experience. My goal is to 
-            further enhance my personal skills and knowledge in UI/UX and art, in order to fully deliver that 
-            experience to you.
-          </div>
-        </div>
+      <div className="intro">
+        <h1>Hello there!</h1>
+        <h2>My name is Anh Dang.</h2>
       </div>
-      <img src={require('../assets/celestial_clash_icon.png')}></img>
+      <p>
+        I am passionate about UI/UX design and art! I believe expressing oneself by creating interactable 
+        yet stunning interfaces for all kinds of people to enjoy is a wonderful experience. My goal is to 
+        further enhance my personal skills and knowledge in UI/UX and art, in order to fully deliver that 
+        experience to you.
+      </p>
     </div>
   )
 }
