6d1f3e35c9e513734d0505859cb04dd8d43ad098
anhhdangg01 <dangaq1@uci.edu>
2025-08-16 21:52:56 -0700

    Added a hamburger menu that appears when the screen is too small for the tabs to fit

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Hamburger from 'hamburger-react';
 import '../styles/navigator.css';
 
 function Navigator() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div className="navigator">
-      <ul>
-        <li>
-          <a class="nav-link" href="#home">
-            Home
-            </a></li>
-        <li>
-          <a class="nav-link" href="#education">
-            Education
-            </a></li>
-        <li>
-          <a class="nav-link" href="#skills">
-            Skills
-            </a></li>
-        <li>
-          <a class="nav-link" href="#projects">
-            Projects
-            </a></li>
-        <li>
-          <a class="nav-link" href="#games">
-            Games
-            </a></li>
-        <li>
-          <a class = "nav-link" href="#experience">
-            Experience
-            </a></li>
-        <li>
-          <a class="nav-link" href="#contact">
-            Contact
-            </a></li>
-      </ul>
+    <div className="header">
+      <div className="navigator">
+        <ul>
+          <li>
+            <a class="nav-link" href="#home">
+              Home
+              </a></li>
+          <li>
+            <a class="nav-link" href="#education">
+              Education
+              </a></li>
+          <li>
+            <a class="nav-link" href="#skills">
+              Skills
+              </a></li>
+          <li>
+            <a class="nav-link" href="#projects">
+              Projects
+              </a></li>
+          <li>
+            <a class="nav-link" href="#games">
+              Games
+              </a></li>
+          <li>
+            <a class = "nav-link" href="#experience">
+              Experience
+              </a></li>
+          <li>
+            <a class="nav-link" href="#contact">
+              Contact
+              </a></li>
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Hamburger toggled={isOpen} toggle={setOpen} color="#CBD5E1" distance="lg" rounded />
+      </div>
     </div>
+
   )
 }
 
