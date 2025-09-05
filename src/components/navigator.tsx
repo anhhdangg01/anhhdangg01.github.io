8caea0a2639567ec0562322a1581f82cf705318d
anhhdangg01 <dangaq1@uci.edu>
2025-09-05 00:06:25 -0700

    Added functional hamburger menu that only appears in mobile devices

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -1,46 +1,40 @@
 import React, { useState } from 'react';
 import Hamburger from 'hamburger-react';
 import '../styles/navigator.css';
+import routes_data from '../data/routes.json';
+
+const routes = routes_data.routes;
 
 function Navigator() {
   const [isOpen, setOpen] = useState(false);
 
+  const getRoutes = () => {
+    return(
+      <ul>
+        {routes.map((route) => {
+          return(
+            <li>
+              <a class="nav-link" href={`#${route.route}`} onclick={() => setOpen((prev) => !prev)}>
+                <span>{route.title}</span>
+                </a></li>
+          )
+        })}
+      </ul>
+    )
+  }
+
   return (
     <div className="header">
       <div className="navigator">
-        <ul>
-          <li>
-            <a class="nav-link" href="#home">
-              Home
-              </a></li>
-          <li>
-            <a class="nav-link" href="#education">
-              Education
-              </a></li>
-          <li>
-            <a class="nav-link" href="#skills">
-              Skills
-              </a></li>
-          <li>
-            <a class="nav-link" href="#projects">
-              Projects
-              </a></li>
-          <li>
-            <a class="nav-link" href="#games">
-              Games
-              </a></li>
-          <li>
-            <a class="nav-link" href="#experiences">
-              Experience
-              </a></li>
-          <li>
-            <a class="nav-link" href="#contact">
-              Contact
-              </a></li>
-        </ul>
+        {getRoutes()}
       </div>
       <div className="hamburger">
         <Hamburger toggled={isOpen} toggle={setOpen} color="#CBD5E1" distance="lg" rounded />
+        {isOpen && (
+          <div className="mobile-nav">
+            {getRoutes()}
+          </div>
+        )}
       </div>
     </div>
 
